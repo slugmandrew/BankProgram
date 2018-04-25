ad500e867e420139d271e4eed8a90ef6a358b1a7
Drew Spencer <slugmandrew@gmail.com>
2018-04-25 11:04:58 +0100

    Added contructor

--- BankProgram/BankAccount.cs
+++ BankProgram/BankAccount.cs
@@ -8,6 +8,12 @@ namespace BankProgram
         public string Owner { get; set; }
         public decimal Balance { get; }
 
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
 
