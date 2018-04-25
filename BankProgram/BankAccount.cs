7fcf9897dbb397a2e4acfbb734efd38840acdfd4
Drew Spencer <slugmandrew@gmail.com>
2018-04-25 11:17:37 +0100

    BankAccount now has number generated from seed

--- BankProgram/BankAccount.cs
+++ BankProgram/BankAccount.cs
@@ -8,10 +8,17 @@ namespace BankProgram
         public string Owner { get; set; }
         public decimal Balance { get; }
 
+        // seed for generating account numbers
+        private static int accountNumberSeed = 1234567890;
+
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
             this.Balance = initialBalance;
+
+            // set the account number and increment seed
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
--- BankProgram/Program.cs
+++ BankProgram/Program.cs
@@ -6,8 +6,15 @@ namespace BankProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Drew Spencer", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
 
+            WaitForTermination();
+        }
+
+        private static void WaitForTermination()
+        {
+            Console.WriteLine("\nPress any key to exit...");
             Console.ReadLine();
         }
     }
