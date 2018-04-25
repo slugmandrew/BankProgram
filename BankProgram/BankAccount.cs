8509e964e1fe0ec5fc25bf8963baea915e40dbfc
Drew Spencer <slugmandrew@gmail.com>
2018-04-25 12:45:07 +0100

    Added basic Transaction functionality

--- BankProgram/BankAccount.cs
+++ BankProgram/BankAccount.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BankProgram
 {
@@ -6,29 +7,60 @@ namespace BankProgram
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
 
         // seed for generating account numbers
         private static int accountNumberSeed = 1234567890;
 
+        private List<Transaction> allTransactions = new List<Transaction>();
+
         public BankAccount(string name, decimal initialBalance)
         {
+            // set owner
             this.Owner = name;
-            this.Balance = initialBalance;
 
             // set the account number and increment seed
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+
+            // set the initial balance by adding the first deposit
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
 
     }
